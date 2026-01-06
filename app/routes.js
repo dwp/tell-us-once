e5f6f79ced3d480c3ca1d8a6aa1ad3388fd142fd
Neil Holroyd <neil.holroyd2@engineering.digital.dwp.gov.uk>
2026-01-06 10:12:53 +0000

    First push
    GitOrigin-RevId: a86f75a1058f631412738681573f247ff519d994

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,5 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+const radioButtonRedirect = require('radio-button-redirect')
+router.use(radioButtonRedirect)
