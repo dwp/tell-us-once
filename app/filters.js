d0d5251fda198f2a8f503b8e27bbc98d0b2b65a1
Ashley Wheat <ashley.wheat@engineering.digital.dwp.gov.uk>
2026-04-29 12:05:36 +0100

    Updating content on the pensions flow following UR
    GitOrigin-RevId: c3c547d38076ce74e4b0a5e52a9d419fbf04c0cf

--- app/filters.js
+++ app/filters.js
@@ -2,7 +2,7 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
 
-// Titlecase: first letter of each word
+// Format as titlecase
 addFilter('titlecase', function (str) {
   if (typeof str !== 'string') {
     return ''
@@ -13,6 +13,7 @@ addFilter('titlecase', function (str) {
   })
 });
 
+// Format long form dates
 addFilter("date", (date) => { 
   const value = date ?? new Date(); 
   
@@ -21,4 +22,15 @@ addFilter("date", (date) => {
     month: "long", 
     year: "numeric", 
   }); 
+});
+
+
+// Format NI numebers as QQ 11 22 33 C
+addFilter('formatNiNumber', function (ni) {
+  if (!ni) return ni;
+
+  return ni.replace(
+    /^([A-Z]{2})(\d{2})(\d{2})(\d{2})([A-Z])$/,
+    '$1 $2 $3 $4 $5'
+  );
 });
